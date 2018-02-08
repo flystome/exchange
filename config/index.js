f15e539d214384f577cad65c8c4b05908ffc2eec
youclavier <youclavier@gmail.com>
2018-02-08 19:47:57 +0800

    add_proxy

--- config/index.js
+++ config/index.js
@@ -10,7 +10,12 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      "/settings/member_data": {
+        target: "http://192.168.1.110:3000",
+        changeOrigin: true
+      }
+    },
 
     // Various Dev Server settings
     host: '0.0.0.0', // can be overwritten by process.env.HOST
