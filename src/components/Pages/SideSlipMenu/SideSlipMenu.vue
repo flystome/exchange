4fd0c9982d3a87205ab6078398738bc4e072a1c9
hazlank <401840614@qq.com>
2018-03-20 19:57:48 +0800

    update sideslip

--- src/components/Pages/SideSlipMenu/SideSlipMenu.vue
+++ src/components/Pages/SideSlipMenu/SideSlipMenu.vue
@@ -2,33 +2,35 @@
   <transition name='SideSlipMenu'>
     <div class='btc-SideSlipMenu' v-if='SideSlipMenu' @click='SideSlipMenuDisplay(false)'>
       <div>
-        <ul>
+        <ul @click.stop>
           <li class='btc-marginL30 btc-marginT30'>
             {{ loginData.show_name }}
           </li>
           <li>
-            <router-link :to="`${ROUTER_VERSION}/currency/deposit`">
+            <span @click="goPath(`/currency/deposit`)">
               <img src='~Img/sideslip-assets.png'>
               {{ $t('side_slip_menu.asset_management') }}
-            </router-link>
+            </span>
           </li>
           <li class='btc-paddingB0'>
-            <img src='~Img/sideslip-consulting.png'> {{ $t('side_slip_menu.consulting_center') }}
-            <span class="btc-right">
-              <img src="~Img/triangle-down.png">
-              <img src="~Img/triangle-up.png">
-            </span>
-            <ul>
+            <div @click="Consulting = !Consulting">
+              <img src='~Img/sideslip-consulting.png'> {{ $t('side_slip_menu.consulting_center') }}
+              <span class="btc-fr btc-marginR20">
+                <img v-if="Consulting" src="~Img/triangle-down.png">
+                <img v-else src="~Img/triangle-up.png">
+              </span>
+            </div>
+            <ul v-if="Consulting">
               <li class='btc-paddingT60 btc-marginL30'>
                 <a :href="`${HOST_URL}/xchg`">
                   {{ $t('side_slip_menu.announcement') }}
                 </a>
               </li>
               <li class='btc-marginL30'>
-                FAQ
+                <a :href="`${HOST_URL}/`">FAQ</a>
               </li>
               <li class='btc-marginL30'>
-                {{ $t('side_slip_menu.news') }}
+                <a :href="`${HOST_URL}/conversations`">{{ $t('side_slip_menu.news') }}</a>
               </li>
             </ul>
           </li>
@@ -36,13 +38,16 @@
             <img src='~Img/sideslip-myaccount.png'> {{ $t('side_slip_menu.my_account') }}
           </li>
           <li>
-            <img src='~Img/sideslip-lang.png'> {{ $t('side_slip_menu.language') }}
-            <ul>
-              <li class='btc-paddingT60 btc-marginL30'>
-                正體中文
-              </li>
-              <li class='btc-marginL30'>
-                English
+            <div @click="Lang = !Lang">
+              <img src='~Img/sideslip-lang.png'> {{ $t('side_slip_menu.language') }}
+              <span class="btc-fr btc-marginR20">
+                <img v-if="Lang" src="~Img/triangle-down.png">
+                <img v-else src="~Img/triangle-up.png">
+              </span>
+            </div>
+            <ul v-if="Lang" class="btc-marginT60">
+              <li v-for="data in locale" @click="changeLang(data.language)" :key="data.name" class='btc-marginL30'>
+                {{ data.name }}
               </li>
             </ul>
           </li>
@@ -62,16 +67,51 @@ export default {
   data () {
     return {
       HOST_URL: process.env.HOST_URL,
-      ROUTER_VERSION: process.env.ROUTER_VERSION
+      ROUTER_VERSION: process.env.ROUTER_VERSION,
+      Consulting: false,
+      Lang: false,
+      locale: [{
+        language: 'zh-TW',
+        name: '正體中文'
+      },
+      {
+        language: 'en',
+        name: 'English'
+      }]
     }
   },
   methods: {
-    ...mapMutations(['SideSlipMenuDisplay'])
+    changeLang (str) {
+      this.SideSlipMenuDisplay(false)
+      this.ChangeLanguage(str)
+      this.$i18n.locale = str
+      this._post({
+        url: '/settings/get_language.json',
+        headers: {
+          'DataType': 'application/json;charset=utf-8'
+        },
+        data: {
+          'content_language': str
+        }
+      })
+    },
+    goPath (path, status, href) {
+      this.SideSlipMenuDisplay(false)
+      if (status) {
+        return
+      }
+      if (href) {
+        location.href = `${this.HOST_URL}${this.ROUTER_VERSION}${path}`
+      }
+      this.$router.push({
+        path: `${this.ROUTER_VERSION}${path}`
+      })
+    },
+    ...mapMutations(['SideSlipMenuDisplay', 'ChangeLanguage'])
   },
   computed: mapState(['SideSlipMenu', 'loginData'])
 }
 </script>
-
 <style lang='scss' scoped>
   @import './SideSlipMenu.scss'
 </style>
