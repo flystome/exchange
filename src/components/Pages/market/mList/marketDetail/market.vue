7e131d1f85f05fb347bb6d8168748d734ae8ca11
luo <flystome@foxmail.com>
2018-03-27 21:39:07 +0800

    title

--- src/components/Pages/market/mList/marketDetail/market.vue
+++ src/components/Pages/market/mList/marketDetail/market.vue
@@ -176,7 +176,7 @@ export default {
       } else if (index === 1) {
         this.$router.push({path: `${this.ROUTER_VERSION}/markets/${this.curmarket}/trades`})
       } else if (index === 2) {
-        this.$router.push({path: `${this.ROUTER_VERSION}/markets/${this.curMarket}/orders`})
+        this.$router.push({path: `${this.ROUTER_VERSION}/markets/${this.curmarket}/orders`})
       }
     },
     addFavorite: function () {
