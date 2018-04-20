18d42eaeccadb1f3934e18570b2d6c58770ce49b
luo <flystome@foxmail.com>
2018-04-20 15:58:15 +0800

    time format

--- src/components/Pages/Exchange/order/order.vue
+++ src/components/Pages/Exchange/order/order.vue
@@ -323,6 +323,18 @@ export default {
         this.sellVolume = this.accounts[this.market.quote_currency].balance * percent
         this.handleVol(this.sellVolume, 'sell')
       }
+    },
+    showNotice () {
+      Notification.requestPermission((permit) => {
+        if (permit === 'granted') {
+          var notification = new Notification('This is a Notice', {
+            dir: 'auto',
+            icon: '/static/img/ok.svg',
+            body: 'hahahah',
+            renotify: true
+          })
+        }
+      })
     }
   }
 }
